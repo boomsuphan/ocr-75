f6b1c73ca6fd31592acf67bc5e456b9c96a73098
EBenze <52275898+ebenze31@users.noreply.github.com>
2025-12-20 15:14:58 +0700

    ..

--- resources/views/layouts/theme.blade.php
+++ resources/views/layouts/theme.blade.php
@@ -17,6 +17,8 @@
 
     <!-- icon -->
     <link href="https://kit-pro.fontawesome.com/releases/v6.7.0/css/pro.min.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="{{ asset('theme/assets/img/logo/logo-cs.png') }}" type="image/x-icon" />
   </head>
   <body>
     <!--[if lte IE 9]>
@@ -56,8 +58,8 @@
             <div class="row align-items-center">
               <div class="col-lg-12">
                 <nav class="navbar navbar-expand-lg">
-                  <a class="navbar-brand" href="index.html">
-                    <img src="assets/img/logo/logo.svg" alt="Logo" />
+                  <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img src="{{ asset('theme/assets/img/logo/logo-cs.png') }}" width="50" />
                   </a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent6" aria-controls="navbarSupportedContent6" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="toggler-icon"></span>
@@ -86,10 +88,10 @@
                     </ul>
                   </div>
                   
-                  <div class="header-action d-flex">
+                  <!-- <div class="header-action d-flex">
                     <a href="#0"> <i class="lni lni-cart"></i> </a>
                     <a href="#0"> <i class="lni lni-alarm"></i> </a>
-                  </div>
+                  </div> -->
                   <!-- navbar collapse -->
                 </nav>
                 <!-- navbar -->
@@ -104,11 +106,12 @@
       <!-- ========================= header-6 end ========================= -->
     </section>
     <!-- ========================= hero-section-wrapper-6 end ========================= -->
+    <div style="height: 100px;"></div>
   <main class="py-4">
             @yield('content')
     </main>
     <!-- ========================= footer style-4 start ========================= -->
-    <footer class="footer footer-style-4">
+    <footer class="footer footer-style-4" style="padding-top:10px !important;">
       <div class="container">
         <div class="copyright-wrapper wow fadeInUp" data-wow-delay=".2s">
           <p>Design and Developed by <a href="https://uideck.com" rel="nofollow" target="_blank">UIdeck</a> Built-with <a href="https://uideck.com" rel="nofollow" target="_blank">Lindy UI Kit</a>. Distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a></p>
